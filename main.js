6a3df99ab39db29a8944a8f4ea41400941325091
Naufalhaykal33 <haykalmuhammad881@gmail.com>
2024-11-21 00:57:01 +0700

    add darkmode

--- main.js
+++ main.js
@@ -19,14 +19,45 @@ const sr = ScrollReveal({
   origin: 'top',
   distance: '65px',
   duration: 1000,
-  delay: 325,
+  delay: 255,
   reset: true // Animation repeat
 })
 
 /* Scrollreveal Animation */
 sr.reveal(`.section__pic-container, .title, .section_text_p1, .footer_title`)
-sr.reveal(`.section_text_p2`, {delay: 520})
+sr.reveal(`.section_text_p2`, {delay: 420})
 sr.reveal(`.about-containers`, {interval: 100})
 sr.reveal(`.icon`, {origin: 'bottom'})
-sr.reveal(`.btn-color-2, .text-container, .details-container, .skills_content, .copyright`, {origin: 'left'}, {interval: 100})
+sr.reveal(`.btn-color-2, .text-container, .details-container, .skills_content`, {origin: 'left'}, {interval: 100})
 sr.reveal(`.btn-color-1, .info__title, .social_icon`, {origin: 'right'}, {interval: 100})
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'light' : 'dark'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-clear-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-clear-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+function toggleTheme() {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+}
